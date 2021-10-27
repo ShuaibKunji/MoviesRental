1610a58e9c0defcfafb08cdb35bb7fe3de857d0a
Shuaib Kunji <shuaibkunji@gmail.com>
2021-10-27 09:38:16 +0530

    Add a new class
    Also added a new controller and a view to test connections

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movies_Rental.Models;
+
+namespace Movies_Rental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Interstellar"};
+            return View(movie);
+        }
+    }
+}
